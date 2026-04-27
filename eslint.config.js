5b3f21b7f08c447903d02e8456c65b1affdda79b
Andrew Jensen <andrewjensen90@gmail.com>
2026-04-27 15:20:51 -0600

    Reject console usage, only allow output in canonical locations

--- eslint.config.js
+++ eslint.config.js
@@ -12,6 +12,7 @@ export default defineConfig(
   {
     files: ["**/*.{ts,mts,cts}"],
     rules: {
+      "no-console": "error",
       "@typescript-eslint/no-unused-vars": [
         "error",
         {
@@ -19,6 +20,38 @@ export default defineConfig(
           varsIgnorePattern: "^_",
           caughtErrorsIgnorePattern: "^_",
           destructuredArrayIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  {
+    // Ban `process.stdout.write` and `process.stderr.write` outside of canonical locations
+    files: ["src/**/*.ts"],
+    ignores: [
+      // Canonical locations where we WANT to call these functions
+      "src/renderers.ts",
+      "src/commandHelpers.ts",
+      "src/logger.ts",
+      "src/commands/studio/queryRendering.ts",
+      "src/commands/studio/query.ts",
+      // Tests and test helpers
+      "**/*.test.ts",
+      "src/testSetup.ts",
+    ],
+    rules: {
+      "no-restricted-syntax": [
+        "error",
+        {
+          selector:
+            "CallExpression[callee.type=MemberExpression][callee.object.type=MemberExpression][callee.object.object.name=process][callee.object.property.name=stdout][callee.property.name=write]",
+          message:
+            "Use renderJson or renderRichText from renderers.ts instead of process.stdout.write().",
+        },
+        {
+          selector:
+            "CallExpression[callee.type=MemberExpression][callee.object.type=MemberExpression][callee.object.object.name=process][callee.object.property.name=stderr][callee.property.name=write]",
+          message:
+            "Use the shared logger, renderRichText, or the other allowlisted output sites instead of process.stderr.write().",
         },
       ],
     },
