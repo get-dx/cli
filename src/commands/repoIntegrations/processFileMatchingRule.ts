4ea4595c2dd442b0dbe27284f9ad29f6bfa13f4c
Andrew Jensen <andrewjensen90@gmail.com>
2026-05-06 21:56:45 -0600

    Setup tree-sitter-python, parse source code

--- src/commands/repoIntegrations/processFileMatchingRule.ts
+++ src/commands/repoIntegrations/processFileMatchingRule.ts
@@ -1,7 +1,12 @@
+import { fileURLToPath } from "node:url";
 import { Command } from "commander";
+import { Language, Parser } from "web-tree-sitter";
 
 import { getContext, wrapAction } from "../../commandHelpers.js";
 import { buildRuntime } from "../../runtime.js";
+import { CliError } from "../../errors.js";
+
+const DIST_DIR = fileURLToPath(new URL("../../../dist", import.meta.url));
 
 const PYTHON_SOURCE = `
 def my_fun_function():
@@ -17,9 +22,26 @@ export function processFileMatchingRuleCommand() {
         const context = getContext(command);
         const runtime = buildRuntime(context);
 
-        console.log("Hello world");
-
+        console.log("Source:");
         console.log(PYTHON_SOURCE);
+
+        console.log("Setting up tree-sitter...");
+        await Parser.init();
+        const parser = new Parser();
+        const LANGUAGE_PYTHON = await Language.load(
+          `${DIST_DIR}/tree-sitter-python.wasm`,
+        );
+        parser.setLanguage(LANGUAGE_PYTHON);
+
+        console.log("Parsing...");
+        const tree = parser.parse(PYTHON_SOURCE);
+
+        if (!tree) {
+          throw new CliError("Failed to parse source code");
+        }
+
+        console.log("Tree:");
+        console.log(tree.rootNode.toString());
       }),
     );
 }
