a101ca39eacec99cf5a0fb149e3c7fc0fc97b3be
Andrew Jensen <andrewjensen90@gmail.com>
2026-05-06 22:10:53 -0600

    Parse Typescript/TSX too

--- src/commands/repoIntegrations/processFileMatchingRule.ts
+++ src/commands/repoIntegrations/processFileMatchingRule.ts
@@ -13,6 +13,14 @@ def my_fun_function():
   print "Hello world"
 `.trim();
 
+const TSX_SOURCE = `
+import React from "react";
+
+function MyComponent() {
+  return <div>Hello world</div>;
+}
+`.trim();
+
 export function processFileMatchingRuleCommand() {
   return new Command()
     .name("processFileMatchingRule")
@@ -22,26 +30,47 @@ export function processFileMatchingRuleCommand() {
         const context = getContext(command);
         const runtime = buildRuntime(context);
 
-        console.log("Source:");
+        console.log("Python source:");
         console.log(PYTHON_SOURCE);
+        await parsePython(PYTHON_SOURCE);
 
-        console.log("Setting up tree-sitter...");
-        await Parser.init();
-        const parser = new Parser();
-        const LANGUAGE_PYTHON = await Language.load(
-          `${DIST_DIR}/tree-sitter-python.wasm`,
-        );
-        parser.setLanguage(LANGUAGE_PYTHON);
-
-        console.log("Parsing...");
-        const tree = parser.parse(PYTHON_SOURCE);
-
-        if (!tree) {
-          throw new CliError("Failed to parse source code");
-        }
-
-        console.log("Tree:");
-        console.log(tree.rootNode.toString());
+        console.log("TSX Source:");
+        console.log(TSX_SOURCE);
+        await parseTSX(TSX_SOURCE);
       }),
     );
+}
+
+async function parsePython(source: string) {
+  await Parser.init();
+
+  const pythonParser = new Parser();
+  const LANGUAGE_PYTHON = await Language.load(
+    `${DIST_DIR}/tree-sitter-python.wasm`,
+  );
+  pythonParser.setLanguage(LANGUAGE_PYTHON);
+
+  const tree = pythonParser.parse(source);
+  if (!tree) {
+    throw new CliError("Failed to parse source code");
+  }
+
+  console.log("Python AST:");
+  console.log(tree.rootNode.toString());
+}
+
+async function parseTSX(source: string) {
+  await Parser.init();
+
+  const tsxParser = new Parser();
+  const LANGUAGE_TSX = await Language.load(`${DIST_DIR}/tree-sitter-tsx.wasm`);
+  tsxParser.setLanguage(LANGUAGE_TSX);
+
+  const tree = tsxParser.parse(source);
+  if (!tree) {
+    throw new CliError("Failed to parse source code");
+  }
+
+  console.log("TSX AST:");
+  console.log(tree.rootNode.toString());
 }
